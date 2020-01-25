1addadd340b68b2bca6aa5e8f45ecb8ca3fa8b4e
andreanascimento9 <55623244+andreanascimento9@users.noreply.github.com>
2020-01-25 20:17:35 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-$(document).ready(function(){
-    $('#enviar').click(function() {
+//$(document).ready(function(){
+//    $('#enviar').click(function() {
 
-        $("input:checked").each(function() {
+//        $("input:checked").each(function() {
         
-         var a = $('#resultado').append($(this).attr("value"))
+//         var a = $('#resultado').append($(this).attr("value"))
        
-        });
+ //       });
       
-      });
-})
+ //     });
+//})
 
-function contador() {
-    var res = document.getElementById('resultado')
-    console.log(res)
-    console.log(typeof(res[0]))
+function check() {
     
-    
+   var checar = document.forms[0];
+   var i
+   var txt = " "
+   for(i = 0; i < checar[i]; i++){
+       if (checar[i].checked) {
+           txt = txt + checar[i].value+ " "
+       }
+       console.log(checar)
+   }
+
+    console.log(checar)
     
     
      
